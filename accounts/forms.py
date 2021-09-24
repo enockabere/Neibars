c4088fb512af292aa623fefa4291c53a345db180
enockabere <maebaenock95@gmail>
2021-09-24 21:36:55 +0300

    create registration form

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class  Meta:
+        model = User
+        fields = ("username","password1","password2")
+    def __init__(self, *args, **kwargs):
+        super(NewUserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+    def save(self,commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
