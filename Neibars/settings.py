bde67e732a0d5716c94246d437f4a4f6e30e6cca
enockabere <maebaenock95@gmail>
2021-09-25 00:24:48 +0300

    create email function

--- Neibars/settings.py
+++ Neibars/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,6 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Neibars.wsgi.application'
 
+
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
