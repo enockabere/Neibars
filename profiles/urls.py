5ad76d99a87e0077af444a28d136b4dc5111b4f0
enockabere <maebaenock95@gmail>
2021-09-25 23:35:45 +0300

    create post forms

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from  . import views
 
 urlpatterns = [
     path('pro', views.create_account,name="create_pro"),
+    path('dashboard', views.dashboard,name="dash"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def create_account(request):
     return render(request,template_name="main/create.html")
+def dashboard(request):
+    return render(request,template_name="main/dashboard.html")
