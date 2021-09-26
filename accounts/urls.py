a15d5f1eb92d563d003d5a815df8e25e4b602541
enockabere <maebaenock95@gmail>
2021-09-26 03:47:02 +0300

    create logout view function

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_request,name="register"),
     path('', views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
     
 ]
